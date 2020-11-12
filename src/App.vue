35bade451d35f1feaad2bca3cdfe803a93825bd0
ITIStomoya06 <35499042+tomoya06@users.noreply.github.com>
2020-11-13 00:46:43 +0800

    add background component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
-    <router-view />
+    <Home />
   </div>
 </template>
+<script>
+import Home from "./views/Home.vue";
+export default {
+  components: { Home },
+};
+</script>
 <style lang="less" scoped>
 #app {
   width: 100%;
